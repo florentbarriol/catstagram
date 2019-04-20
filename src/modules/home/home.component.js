a640e7b4653f6f033c9f79fc75d6ffd2d9b01660
Flo Barriol <florentbarriol@users.noreply.github.com>
2019-04-20 09:48:51 +0200

    add component to home page

--- src/modules/home/home.component.js
+++ src/modules/home/home.component.js
@@ -1,7 +1,21 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import { ROOT_API } from '../../constants';
 import { ErrorBoundary } from '../../components/errorBoundary.component';
-import Grid from '../../components/grid.component';
+import { Footer } from '../../components/footer.component';
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+const Aside = styled.aside`
+  max-width: 293px;
+  flex: 0 0 auto;
+`;
+
+const Post = styled.article`
+  margin: 0 0 1.5rem;
+`;
+const Img = styled.img``;
 
 const useFetch = () => {
   const [posts, setPosts] = useState([]);
@@ -25,9 +39,20 @@ const useFetch = () => {
 const Home = () => {
   const { posts } = useFetch();
   return (
-    <ErrorBoundary>
-      <Grid items={posts} />
-    </ErrorBoundary>
+    <Wrapper>
+      <main>
+        {posts.map(post => (
+          <ErrorBoundary>
+            <Post>
+              <Img src={post.image} />
+            </Post>
+          </ErrorBoundary>
+        ))}
+      </main>
+      <Aside>
+        <Footer />
+      </Aside>
+    </Wrapper>
   );
 };
 
